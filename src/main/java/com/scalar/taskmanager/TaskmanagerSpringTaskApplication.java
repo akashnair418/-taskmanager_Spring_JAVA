634f7d932b6f1c89190edf4ed219565c96576727
Acer <nairakash418@gmail.com>
2025-04-04 22:42:09 +0530

    Initial commit

---
+++ src/main/java/com/scalar/taskmanager/TaskmanagerSpringTaskApplication.java
@@ -0,0 +1,13 @@
+package com.scalar.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerSpringTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerSpringTaskApplication.class, args);
+	}
+
+}
